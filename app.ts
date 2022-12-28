135577e351d466d36b0f94701128f25a5c9d5934
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-28 18:26:42 +0100

    Create /os API

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
-// import http from "http";
 import express, { Request, Response, NextFunction } from "express";
+import os from "os";
 
 // const hostname = "127.0.0.1";
 const app = express();
@@ -17,6 +17,32 @@ const port = 3000;
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json("Hello World !");
+});
+app.get("/os", (req: Request, res: Response, next: NextFunction) => {
+  let osCpus: os.CpuInfo[] = os.cpus();
+  const osTotalmem = os.totalmem();
+  const osFreemem = os.freemem();
+
+  osCpus = osCpus.map((cpuItem) => {
+    return {
+      ...cpuItem,
+      times: {
+        ...cpuItem.times,
+        user: cpuItem.times.user / 1000, // From milliseconds to seconds
+        sys: cpuItem.times.sys / 1000, // From milliseconds to seconds
+        idle: cpuItem.times.idle / 1000, // From milliseconds to seconds
+      },
+    };
+  });
+
+  const results = {
+    osCpus,
+    osTotalmem: osTotalmem * Math.pow(10, -9) + " GB", // bytes to GB
+    osFreemem: osFreemem * Math.pow(10, -9) + " GB", // bytes to GB
+  };
+  console.log("/os results", results);
+
+  res.status(200).json({ results });
 });
 
 app.listen(port, () => {
