e2ea6d82bde50c0d3de20f348165443a0fe789cf
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-28 16:28:34 +0100

    Install and use Express

--- app.ts
+++ app.ts
@@ -1,14 +1,24 @@
-import http from "http";
+// import http from "http";
+import express, { Request, Response, NextFunction } from "express";
 
-const hostname = "127.0.0.1";
+// const hostname = "127.0.0.1";
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req: any, res: any) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+// * Use http package
+// const server = http.createServer((req: any, res: any) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World");
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json("Hello Wordl !");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
