fd6d9324d16128f6c14ecb63e45cabee7dfbe490
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-18 17:02:04 +0200

    Add command

--- scripts/setup_linux_key.sh
+++ scripts/setup_linux_key.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "================ setup_linux_key.sh - start ================"
 key=$(openssl rand -hex 16)
 key_dir="${HOME}/.HelloWorldGoAgent"
 mkdir -p "${key_dir}"
 echo "${key}" > "${key_dir}/PSK_key.txt"
+echo "================ setup_linux_key.sh - end ================"
--- scripts/setup_windows_key.ps1
+++ scripts/setup_windows_key.ps1
@@ -1,8 +1,10 @@
 <powershell>
+Write-Output "================ setup_windows_key.ps1 - start ================"
 $key = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 32 | % {[char]$_})
 $keyDir = "$env:USERPROFILE\.HelloWorldGoAgent"
 if (-not (Test-Path -Path $keyDir)) {
     New-Item -ItemType Directory -Path $keyDir
 }
 Set-Content -Path "${keyDir}\PSK_key.txt" -Value $key
+Write-Output "================ setup_windows_key.ps1 - end ================"
 </powershell>
