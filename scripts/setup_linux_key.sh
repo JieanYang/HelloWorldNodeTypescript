1bee0af2e6f6a67c7af27b235c9b8847f3aeef5c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-18 17:50:03 +0200

    UPdate scripts/setup_linux_key.sh

--- scripts/setup_linux_key.sh
+++ scripts/setup_linux_key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "================ setup_linux_key.sh - start ================"
-key=$(openssl rand -hex 16)
+key=$(sudo openssl rand -hex 16)
 key_dir="${HOME}/.HelloWorldGoAgent"
 mkdir -p "${key_dir}"
 echo "${key}" > "${key_dir}/PSK_key.txt"
