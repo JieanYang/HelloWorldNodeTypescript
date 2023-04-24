4467fc27a4e8252c7fbce89fd68ec3c9b05d602c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-24 17:27:48 +0200

    Remove send key to backend test from scripts/setup_linux_key.sh

--- scripts/setup_linux_key.sh
+++ scripts/setup_linux_key.sh
@@ -12,9 +12,3 @@ json="{
 }"
 echo "${json}" > "${key_dir}/original_metadata.json"
 echo "================ setup_linux_key.sh - end ================"
-
-
-echo "================ send key to backend test - start ================"
-curl -X POST -H "Content-Type: application/json" -d "{\"key\":\"${key}\"}" https://ee5c-2a01-cb08-ad0-f700-e00b-d224-dbfe-bb45.ngrok-free.app/node/api-docs/#/Agent/post_agent_receivePSKKey
-
-echo "================ send key to backend test - end ================"
