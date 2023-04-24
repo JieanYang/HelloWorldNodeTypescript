d71b9c028df2b63236a6c4f8619d50094e3b6644
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-24 16:47:02 +0200

    Add agent/receivePSKKey for backend

--- src/routes/RAgent.ts
+++ src/routes/RAgent.ts
@@ -2,10 +2,32 @@ import { Router } from "express";
 
 export const RAgent = Router();
 
+/**
+ * @openapi
+ * tags:
+ *   name: Agent
+ *   description: Agent routes
+ */
+
 RAgent.route("/receiveHearbeat").get((req, res) => {
   res.status(200).json("OK");
 });
 
+/**
+ * @openapi
+ * /agent/receivePSKKey:
+ *  get:
+ *     description: agent reveive PSK key
+ *     tags:
+ *         - Agent
+ *     responses:
+ *         '200':
+ *             description: Success
+ *         '400':
+ *             description: Missing parameters
+ *         '500':
+ *             description: Error
+ */
 RAgent.route("/receivePSKKey").get((req, res) => {
   res.status(200).json("OK");
 });
