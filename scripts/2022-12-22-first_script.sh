462212ae87c8aa286d01ea14cb58419485cdb95f
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-26 13:48:36 +0200

    Update

--- scripts/2022-12-22-first_script.sh
+++ scripts/2022-12-22-first_script.sh
@@ -2,3 +2,17 @@
 echo "start"
 echo "hello yang"
 echo "end"
+
+while getopts ":f:n:" opt; do
+  case $opt in
+    f) filename="$OPTARG"
+    ;;
+    n) count="$OPTARG"
+    ;;
+    \?) echo "无效的选项 -$OPTARG" >&2
+    ;;
+  esac
+done
+
+echo "输入的文件名是：$filename"
+echo "输入的计数值是：$count"
