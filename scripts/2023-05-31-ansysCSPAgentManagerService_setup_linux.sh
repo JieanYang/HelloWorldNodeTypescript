adcfca4bd46fff0683c461c0e8851e365e9548e3
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-05-31 18:34:01 +0200

    Debug scripts/2023-05-31-ansysCSPAgentManagerService_setup_linux.sh

--- scripts/2023-05-31-ansysCSPAgentManagerService_setup_linux.sh
+++ scripts/2023-05-31-ansysCSPAgentManagerService_setup_linux.sh
@@ -62,22 +62,44 @@ echo $ $OS_SERVICE_MANAGER_APP
 echo $ $AGENT_APP
 # === App path - end ===
 
-OS_SERVICE_MANAGER_APP_BINARY_URL="https://ansys-gateway-development.s3.eu-west-3.amazonaws.com/ansysCSPAgentManagerServiceApp"
-AGENT_APP_BINARY_URL="https://ansys-gateway-development.s3.eu-west-3.amazonaws.com/ansysCSPAgentApp"
+if [ ! -d "$OS_SERVICE_MANAGER_APP_DIR" ]; then
+  mkdir -p "$OS_SERVICE_MANAGER_APP_DIR"
+  echo "Folder created successfully."
+else
+  echo "Folder already exists."
+fi
+
+if [ ! -d "$AGENT_APP_DIR/src" ]; then
+  mkdir -p "$AGENT_APP_DIR/src"
+  echo "Folder created successfully."
+else
+  echo "Folder already exists."
+fi
+
+OS_SERVICE_MANAGER_APP_BINARY_URL="https://ansys-gateway-development.s3.eu-west-3.amazonaws.com/linux/ansysCSPAgentManagerServiceApp"
+OS_SERVICE_MANAGER_APP_INSTALL_SERVICE_URL="https://ansys-gateway-development.s3.eu-west-3.amazonaws.com/linux/install_linux.sh"
+OS_SERVICE_MANAGER_APP_UNINSTALL_SERVICE_URL="https://ansys-gateway-development.s3.eu-west-3.amazonaws.com/linux/uninstall_linux.sh"
+OS_SERVICE_MANAGER_APP_SERVICE_CONFIG_URL="https://ansys-gateway-development.s3.eu-west-3.amazonaws.com/linux/AnsysCSPAgentManagerService.service"
+AGENT_APP_BINARY_URL="https://ansys-gateway-development.s3.eu-west-3.amazonaws.com/linux/ansysCSPAgentApp"
 
 curl -L "$OS_SERVICE_MANAGER_APP_BINARY_URL" -o "$OS_SERVICE_MANAGER_APP"
+curl -L "$OS_SERVICE_MANAGER_APP_INSTALL_SERVICE_URL" -o "$OS_SERVICE_MANAGER_APP_DIR/install_linux.sh"
+curl -L "$OS_SERVICE_MANAGER_APP_UNINSTALL_SERVICE_URL" -o "$OS_SERVICE_MANAGER_APP_DIR/uninstall_linux.sh"
+curl -L "$OS_SERVICE_MANAGER_APP_SERVICE_CONFIG_URL" -o "$OS_SERVICE_MANAGER_APP_DIR/AnsysCSPAgentManagerService.service"
 curl -L "$AGENT_APP_BINARY_URL" -o "$AGENT_APP"
 
 # Add +x permission for two binary files
 cd $OS_SERVICE_MANAGER_APP_DIR
 sudo chmod +x ${OS_SERVICE_MANAGER_APP}
 sudo chmod +x ${AGENT_APP}
+sudo chmod +x "${OS_SERVICE_MANAGER_APP_DIR}/install_linux.sh"
+sudo chmod +x "${OS_SERVICE_MANAGER_APP_DIR}/uninstall_linux.sh"
 
 # Copy ansysCSPAgentApp to OS service manager app folder
 sudo sudo cp ${AGENT_APP} ${OS_SERVICE_MANAGER_APP_DIR}
 
 cd $OS_SERVICE_MANAGER_APP_DIR
-./install_linux.sh
+sudo ./install_linux.sh
 
 
 echo "=== 2023-05-29-ansysCSPAgentManagerService_setup_linux.sh - end ==="
